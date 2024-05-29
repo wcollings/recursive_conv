e520ace2c289378d3c240e52ad7591a59b376201
Will Collings <william.collings@rockets.utoledo.edu>
2024-05-29 13:17:39 -0400

    made a header for ddiff

---
+++ include/ddiff.h
@@ -0,0 +1,16 @@
+#ifndef __DDIFF_H__
+#define __DDIFF_H__
+
+/*
+ * @brief Creates a divided difference table based on the x and y given.
+ * This essentially approximates `n-1` derivatives of y(x).
+ * out[i] -> the ith derivative of this function
+ *
+ * `x` The inputs to the function (must be `num_ele` elements long)
+ * `y` the outputs of the function (must be `num_ele` elements long)
+ * `num_ele` the number of elements in x and y. Also corresponds to greater than the number of derivatives you want to take
+ * Returns:
+ * a table of derivatives (doubles) of length `num_ele`
+*/
+double * divided_difference(double * x,double *y, int num_ele);
+#endif
--- src/ddiff.c
+++ src/ddiff.c
@@ -3,17 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
-/*
- * @brief Creates a divided difference table based on the x and y given.
- * This essentially approximates `n-1` derivatives of y(x).
- * out[i] -> the ith derivative of this function
- *
- * `x` The inputs to the function (must be `num_ele` elements long)
- * `y` the outputs of the function (must be `num_ele` elements long)
- * `num_ele` the number of elements in x and y. Also corresponds to greater than the number of derivatives you want to take
- * Returns:
- * a table of derivatives (doubles) of length `num_ele`
-*/
+#include "../include/ddiff.h"
 double * divided_difference(double * x,double *y, int num_ele) {
 	double * out=malloc(sizeof(double)*num_ele);
 	for (int i=0; i < num_ele; ++i)
