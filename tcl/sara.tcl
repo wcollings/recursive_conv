5d99d1a2831ce07d73bdbc0e0f5487e5b94064ba
Sam-Patil-56 <128252033+Sam-Patil-56@users.noreply.github.com>
2024-08-14 15:32:53 -0400

    Final comb through fixes like proper indexing offsets and making order a calculated value set in Init

--- tcl/sara.tcl
+++ tcl/sara.tcl
@@ -16,7 +16,7 @@
 
 package require qcomplex
 
-proc SARA:Init {instance order k0 k0j p0 p0j p1 p1j p2 p2j p3 p3j q0 q0j q1 q1j q2 q2j q3 q3j} {
+proc SARA:Init {instance k0 k0j p0 p0j p1 p1j p2 p2j p3 p3j q0 q0j q1 q1j q2 q2j q3 q3j} {
     global SARA
 	 global solver
     
@@ -30,6 +30,17 @@ proc SARA:Init {instance order k0 k0j p0 p0j p1 p1j p2 p2j p3 p3j q0 q0j q1 q1j 
     #set SARA($instance,q1) -2
     #set SARA($instance,q2) 1.255
     #set SARA($instance,q3) 1
+	if [qcomplex::real{p0} == 0 && qcomplex::imag{p0} == 0] {
+		puts "p0 is 0, not allowed"
+	} else {
+		set i 1
+	}
+	set order 3
+	while {i < 4} {
+		if [qcomplex::real{p$i} == 0 && qcomplex::imag{p$1} == 0] {
+			set order [$i - 1]
+		}
+	}
 
 	dict set SARA $instance {
 		order $order
@@ -102,69 +113,72 @@ proc SARA:q2 {sigma_i delta_n n} {
 }
 
 
-proc SARA:Step {instance t x} {
-    global SARA
+proc SARA:Step {instance time input}   {   
+	global SARA
 	 global solver
 
+	 set delta_n [expr {$time - [lindex [dict get $solver tt] 0]}]
+	 set xx_Curr [expr {[lindex [dict get $solver xx] 0]+[$delta_n]*[$input - [dict get $solver xx_Prev]]}]
 	 set tempList [list 0 0 0]
-	 set xx_Curr 0
 	 set order [dict get $SARA $instance order]
-	 set final 0
-	 set delta_n [expr {$time - [dict get $solver prev_time]}]
+	 set final [expr {[dict get $SARA k0]*$xx_Curr}]
+	 
 	 for {set i 0} {$i < $order} {incr i} {
 		 set sigma [dict get $SARA $instance "q$i"]
 		 set a [dict get $SARA $instance "p$i"]
-		 set temp [expr {[lindex [dict get $solver "yy"] $i]*[SARA:Phi $sigma delta_n]}]
-		 set xx_Curr [expr {[lindex [dict get $solver xx] 0]+[time - [lindex [dict get $solver tt] 0]]*[$input - [dict get $solver xx_Prev]]}]
-		 set q [[q0] $sigma $delta_n 0]
+		 set temp [expr {[lindex [dict get $solver "yy"] $i]*[SARA:Phi $sigma $delta_n]}]
+		 
+		 set q [[q[$order]] $sigma $delta_n 0]
 		 incr temp [expr{$a*$q*$xx_Curr}] 
 		 for {set j 0} {$j < [$order - 1]} {incr j} {
-			 set q [[q[expr{$j+1}]] $sigma $delta_n [expr{$j+1}]]
+			 set q [[q[$order]] $sigma $delta_n [expr{$j+1}]]
 			 set temp [expr {$temp + $a*$q*[lindex [dict get $solver xx] $j]}]
 		 }
-		 set tempList [lrange [dict get $solver xx] 0 2]
-		 dict set $solver xx [concat xx_Curr tempList]
-		 set tempList [lrange [dict get $solver tt] 0 2]
-		 dict set $solver tt [concat time tempList]
-		 dict set $solver xx_Prev [$input]
 		 dict set $solver yy $i [$temp]
 		 incr final $temp
 
 	 }
+		 set tempList [lrange [dict get $solver xx] 0 2]
+		 dict set $solver xx [concat $xx_Curr tempList]
+		 set tempList [lrange [dict get $solver tt] 0 2]
+		 dict set $solver tt [concat $time tempList]
+		 dict set $solver xx_Prev [$input]
 
-    return $final
+	return $final
+
 }
 
 # TODO: make this save the results
-proc SARA:Accept {instance time input output} {
-    global SARA
+proc SARA:Accept {instance time input} {
+     global SARA
 	 global solver
 
+	 set delta_n [expr {$time - [lindex [dict get $solver tt] 0]}]
+	 set xx_Curr [expr {[lindex [dict get $solver xx] 0]+[$delta_n]*[$input - [dict get $solver xx_Prev]]}]
 	 set tempList [list 0 0 0]
-	 set xx_Curr 0
 	 set order [dict get $SARA $instance order]
-	 set final 0
-	 set delta_n [expr {$time - [dict get $solver prev_time]}]
+	 set final [expr {[dict get $SARA k0]*$xx_Curr}]
+	 
 	 for {set i 0} {$i < $order} {incr i} {
 		 set sigma [dict get $SARA $instance "q$i"]
 		 set a [dict get $SARA $instance "p$i"]
-		 set temp [expr {[lindex [dict get $solver "yy"] $i]*[SARA:Phi $sigma delta_n]}]
-		 set xx_Curr [expr {[lindex [dict get $solver xx] 0]+[time - [lindex [dict get $solver tt] 0]]*[$input - [dict get $solver xx_Prev]]}]
+		 set temp [expr {[lindex [dict get $solver "yy"] $i]*[SARA:Phi $sigma $delta_n]}]
+		 
 		 set q [[q0] $sigma $delta_n 0]
 		 incr temp [expr{$a*$q*$xx_Curr}] 
 		 for {set j 0} {$j < [$order - 1]} {incr j} {
 			 set q [[q[expr{$j+1}]] $sigma $delta_n [expr{$j+1}]]
 			 set temp [expr {$temp + $a*$q*[lindex [dict get $solver xx] $j]}]
 		 }
+		 dict set $solver yy $i [$temp]
+		 incr final $temp
+
+	 }
 		 set tempList [lrange [dict get $solver xx] 0 2]
 		 dict set $solver xx [concat xx_Curr tempList]
 		 set tempList [lrange [dict get $solver tt] 0 2]
 		 dict set $solver tt [concat time tempList]
 		 dict set $solver xx_Prev [$input]
-		 dict set $solver yy $i [$temp]
-		 incr final $temp
-
-	 }
-	 dict set $solver yy_Final [$final]
+	 	 dict set $solver yy_Final [$final]
 
 }
