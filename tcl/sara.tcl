85448f946a0d5eff6982c4d02a58dfc882815b36
Will Collings <william.collings@rockets.utoledo.edu>
2024-08-15 14:07:29 -0400

    debugging

--- tcl/sara.tcl
+++ tcl/sara.tcl
@@ -14,55 +14,46 @@
 # TODO: do we need to do something different if the coefficients are
 #      complex numbers? How do we deal with that?
 
-package require qcomplex
+package require math::complexnumbers
+namespace import ::math::complexnumbers::*
 
 proc SARA:Init {instance k0 k0j p0 p0j p1 p1j p2 p2j p3 p3j q0 q0j q1 q1j q2 q2j q3 q3j} {
-    global SARA
-	 global solver
-    
-    #set SARA($instance,order) 3
-    #set SARA($instance,p0) 1.2
-    #set SARA($instance,p1) -2
-    #set SARA($instance,p2) 1.255
-    #set SARA($instance,p3) 1
-
-    #set SARA($instance,q0) 1.2
-    #set SARA($instance,q1) -2
-    #set SARA($instance,q2) 1.255
-    #set SARA($instance,q3) 1
-	if [qcomplex::real{p0} == 0 && qcomplex::imag{p0} == 0] {
+	global SARA
+	global solver
+	set i 1
+	if {$p0 == 0 && $p0j == 0} {
 		puts "p0 is 0, not allowed"
-	} else {
-		set i 1
 	}
 	set order 3
-	while {i < 4} {
-		if [qcomplex::real{p$i} == 0 && qcomplex::imag{p$1} == 0] {
+	while {$i < 4} {
+		set realpart q$i
+		set imagpart_name [join [list "q" $i "j"] ""]
+		upvar 0 $imagpart_name imagpart
+		if {$realpart == 0 && $imagpart == 0} {
 			set order [$i - 1]
+			break
 		}
+		incr i
 	}
 
-	dict set SARA $instance {
-		order $order
-		k0 [qcomplex::complex $k0 $k0j]
-		p0 [qcomplex::complex $p0 $p0j]
-		p1 [qcomplex::complex $p1 $p1j]
-		p2 [qcomplex::complex $p2 $p2j]
-		p3 [qcomplex::complex $p3 $p3j]
-
-		q0 [qcomplex::complex $q0 $q0j]
-		q1 [qcomplex::complex $q1 $q1j]
-		q2 [qcomplex::complex $q2 $q2j]
-		q3 [qcomplex::complex $q3 $q3j]
-	}
+	set SARA($instance,"order") $order
+	set SARA($instance,"k0") [complex $k0 $k0j]
+	set SARA($instance,"p0") [complex $p0 $p0j]
+	set SARA($instance,"p1") [complex $p1 $p1j]
+	set SARA($instance,"p2") [complex $p2 $p2j]
+	set SARA($instance,"p3") [complex $p3 $p3j]
+	set SARA($instance,"q0") [complex $q0 $q0j]
+	set SARA($instance,"q1") [complex $q1 $q1j]
+	set SARA($instance,"q2") [complex $q2 $q2j]
+	set SARA($instance,"q3") [complex $q3 $q3j]
 	
-	dict set solver num_iter 0
-	dict set solver prev_time 0
-	dict set solver tt {{0 0 0 0}}   
-	dict set solver xx {{0 0 0 0}}	 	;# previous timesteps of xx integral
-	dict set solver xx_Prev 0			;# previous value of xx
-	dict set solver yy {{0 0 0 0}}		;# yy0 to yy3, NOT previous timesteps
-	dict set solver yy_Final
+	set solver($instance,"num_iter") 0
+	set solver($instance,"prev_time") [complex 0 0]
+	set solver($instance,"tt") [list [complex 0 0] [complex 0 0] [complex 0 0] [complex 0 0]]
+	set solver($instance,"xx") [list [complex 0 0] [complex 0 0] [complex 0 0] [complex 0 0]]
+	set solver($instance,"xx_Prev") [complex 0 0]
+	set solver($instance,"yy") [list [complex 0 0] [complex 0 0] [complex 0 0] [complex 0 0]]
+	set solver($instance,"yy_Final") [complex 0 0]
 }
 
 proc SARA:Phi {i n} {
@@ -115,70 +106,74 @@ proc SARA:q2 {sigma_i delta_n n} {
 
 proc SARA:Step {instance time input}   {   
 	global SARA
-	 global solver
+	global solver
 
-	 set delta_n [expr {$time - [lindex [dict get $solver tt] 0]}]
-	 set xx_Curr [expr {[lindex [dict get $solver xx] 0]+[$delta_n]*[$input - [dict get $solver xx_Prev]]}]
-	 set tempList [list 0 0 0]
-	 set order [dict get $SARA $instance order]
-	 set final [expr {[dict get $SARA k0]*$xx_Curr}]
-	 
-	 for {set i 0} {$i < $order} {incr i} {
-		 set sigma [dict get $SARA $instance "q$i"]
-		 set a [dict get $SARA $instance "p$i"]
-		 set temp [expr {[lindex [dict get $solver "yy"] $i]*[SARA:Phi $sigma $delta_n]}]
-		 
-		 set q [[q[$order]] $sigma $delta_n 0]
-		 incr temp [expr{$a*$q*$xx_Curr}] 
-		 for {set j 0} {$j < [$order - 1]} {incr j} {
-			 set q [[q[$order]] $sigma $delta_n [expr{$j+1}]]
-			 set temp [expr {$temp + $a*$q*[lindex [dict get $solver xx] $j]}]
-		 }
-		 dict set $solver yy $i [$temp]
-		 incr final $temp
-
+	puts [lindex $solver($instance,"tt") 0]
+	set delta_n [expr {$time - [lindex $solver($instance,"tt") 0]}]
+	set currx [expr {[lindex $solver($instance,xx) 0]+[$delta_n]*[$input - $solver($instace,xx_Prev)]}]
+	puts $currx
+	set tempList [list 0 0 0]
+	set order $SARA($instance,order)
+	set final [expr {$SARA($instance,"k0")*$currx}]
+	
+	for {set i 0} {$i < $order} {incr i} {
+		set sigma $SARA($instance,"q$i")
+		set a $SARA($instance,"p$i")
+		set temp [expr {[lindex $solver($instance,yy) $i]*[SARA:Phi $sigma $delta_n]}]
+		set qq "SARA:q$order"
+		
+		set q [qq $sigma $delta_n 0]
+		incr temp [expr{$a*$q*$xx_Curr}] 
+		for {set j 0} {$j < [$order - 1]} {incr j} {
+			set q [qq $sigma $delta_n [expr{$j+1}]]
+			set temp [expr {$temp + $a*$q*[lindex $solver($instance,xx) $j]}]
+		}
+		set $solver($instance,yy,$i) [$temp]
+		incr final $temp
 	 }
 		 set tempList [lrange [dict get $solver xx] 0 2]
-		 dict set $solver xx [concat $xx_Curr tempList]
+		 set $solver($instance,xx) [concat $xx_Curr tempList]
 		 set tempList [lrange [dict get $solver tt] 0 2]
-		 dict set $solver tt [concat $time tempList]
-		 dict set $solver xx_Prev [$input]
+		 set $solver($instance,tt) [concat $time tempList]
+		 set $solver($instance,xx_Prev) [$input]
 
 	return $final
 
 }
 
-# TODO: make this save the results
 proc SARA:Accept {instance time input} {
-     global SARA
-	 global solver
+	global SARA
+	global solver
+	set time [complex $time 0]
+	puts [lindex $solver($instance,"tt") 0]
 
-	 set delta_n [expr {$time - [lindex [dict get $solver tt] 0]}]
-	 set xx_Curr [expr {[lindex [dict get $solver xx] 0]+[$delta_n]*[$input - [dict get $solver xx_Prev]]}]
-	 set tempList [list 0 0 0]
-	 set order [dict get $SARA $instance order]
-	 set final [expr {[dict get $SARA k0]*$xx_Curr}]
-	 
-	 for {set i 0} {$i < $order} {incr i} {
-		 set sigma [dict get $SARA $instance "q$i"]
-		 set a [dict get $SARA $instance "p$i"]
-		 set temp [expr {[lindex [dict get $solver "yy"] $i]*[SARA:Phi $sigma $delta_n]}]
-		 
-		 set q [[q0] $sigma $delta_n 0]
-		 incr temp [expr{$a*$q*$xx_Curr}] 
-		 for {set j 0} {$j < [$order - 1]} {incr j} {
-			 set q [[q[expr{$j+1}]] $sigma $delta_n [expr{$j+1}]]
-			 set temp [expr {$temp + $a*$q*[lindex [dict get $solver xx] $j]}]
-		 }
-		 dict set $solver yy $i [$temp]
-		 incr final $temp
+	set delta_n [expr {$time - [lindex $solver($instance,"tt") 0]}]
+	set currx [complex [expr {[lindex $solver($instance,"xx") 0]+$delta_n*($input - $solver($instance,"xx_Prev"))}] 0]
+	set tempList [list 0 0 0]
+	set order $SARA($instance,"order")
+	puts $SARA($instance,"k0")
+	set final [* $SARA($instance,"k0") $currx]
+	
+	for {set i 0} {$i < $order} {incr i} {
+		set sigma $SARA($instance,"q$i")
+		set a $SARA($instance,"p$i")
+		set temp [* [lindex $solver($instance,"yy") $i] [SARA:Phi $sigma $delta_n]]
+		set qq q$order
+		
+		set q [qq $sigma $delta_n 0]
+		incr temp [* $a $q $currx] 
+		for {set j 0} {$j < [$order - 1]} {incr j} {
+			set q [qq $sigma $delta_n [expr{$j+1}]]
+			set temp [expr {$temp + $a*$q*[lindex $solver($instance,xx) $j]}]
+		}
+		set $solver($instance,yy,$i) [$temp]
+		incr final $temp
 
 	 }
-		 set tempList [lrange [dict get $solver xx] 0 2]
-		 dict set $solver xx [concat xx_Curr tempList]
-		 set tempList [lrange [dict get $solver tt] 0 2]
-		 dict set $solver tt [concat time tempList]
-		 dict set $solver xx_Prev [$input]
-	 	 dict set $solver yy_Final [$final]
-
+	set tempList [lrange $solver($instance,xx) 0 2]
+	set solver($instance,xx) [concat xx_Curr tempList]
+	set tempList [lrange $solver($instance,tt) 0 2]
+	set solver($instance,tt) [concat time tempList]
+	set solver($instance,xx_Prev) $input
+	set solver($instance,yy_Final) $final
 }
