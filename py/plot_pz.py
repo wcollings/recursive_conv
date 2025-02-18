a4023b541b2bcf6913094138935ef0a5615bad2f
Will Collings <william.collings@utoledo.edu>
2025-02-18 13:39:56 -0500

    updating plot script

--- py/plot_pz.py
+++ py/plot_pz.py
@@ -5,8 +5,11 @@ from poly import Poly
 from pade import Pade, separate
 from typing import Callable
 from scipy.optimize import curve_fit as cf
+from plotting import figure_wrapper
 
 df=pd.read_csv("results/zbus2.csv")
+ladder=pd.read_csv("results/Y_ladder.csv")
+ladder['w']=ladder.f*2*np.pi
 # df['l']*=1e-9
 # df['r']*=1e-3
 # df['r']+=min(df['r'])
@@ -15,8 +18,8 @@ freq=df.w
 df['k0']=1/df.l
 df['s0']=df.r/df.l
 df['y']=df.k0/(freq+df.s0)
-# lower_idx=0
-lower_idx=178
+lower_idx=0
+# lower_idx=178
 
 def create_pade(n:int,d:int) -> Callable:
 	def inner(*vals):
@@ -46,9 +49,20 @@ if __name__=="__main__":
 	with open("ybus.txt",'w') as fp:
 		fp.write(separate(h23).get_coeff_str())
 	Y=df.k0/(df.f+df.s0)
-	plt.semilogx(freq,df.y,label="Model")
-	plt.semilogx(freq,h23(freq),label="Experimenal")
-	plt.xlabel("Frequency (rad/sec)")
-	plt.ylabel("Admittance (S)")
-	plt.legend()
-	plt.show()
+	with figure_wrapper(interactive=True,outf="Y_vs_ladder.png") as fw:
+		fw.slogx(freq,df.y,name="Experiment",color="black")
+		fw.slogx(ladder.w,ladder.y,name="Ladder Network",color="#0072BD")
+		fw.slogx(freq,h23(freq),name="Model",linestyle="--",color="#cc0000")
+		fw.xlim=1e4,1e9
+		fw.ylim=-7,166
+		fw.set_fontsize(20)
+		fw.set_labels("Frequency (rad/sec)","|Admittance| (S)")
+
+	# plt.semilogx(freq,df.y,label="Model")
+	# plt.semilogx(ladder.w,ladder.y,label="Ladder Network")
+	# plt.semilogx(freq,h23(freq),label="Experimenal")
+	# plt.xlabel("Frequency (rad/sec)")
+	# plt.ylabel("Admittance (S)")
+	# plt.grid()
+	# plt.legend()
+	# plt.show()
